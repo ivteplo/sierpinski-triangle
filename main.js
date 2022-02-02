97b58e6da056eb27fe011720fce4af82c50d8e73
ivteplo <iteplov7@gmail.com>
2022-02-02 22:52:36 +0200

    Make it animated!

--- main.js
+++ main.js
@@ -54,35 +54,30 @@ function* pascalTriangle (from, to) {
 
 
 // The number of rows in the triangle minus 1
-let rowsCount = 128
+const rowsCount = 64
 
 // Size of a 'cell' in the triangle
-let squareSize = 3
+const defaultSquareSize = 4
 
 
-// Function that is called on each frame
-const draw = () => {
-    const { width, height } = canvas
-    
-    // Fill the background with black
-    context.fillStyle = 'black'
-    context.strokeStyle = 'transparent'
-    context.fillRect(0, 0, width, height)
+const widthOfLine = (lineNumber, squareSize) =>
+    (lineNumber + 1) * squareSize
 
+const heightOfTriangle = (linesCount, squareSize) =>
+    linesCount * squareSize
+
+const bigInt = (number) => BigInt(Math.floor(number))
+
+
+const drawTriangleAt = ({ x, y, startingRow = 1, rowsCount, squareSize }) => {
     // Set the fill color of our triangle
     context.fillStyle = 'white'
 
-    // Coordinates of the center of the screen
-    const centerX = width / 2
-    const centerY = height / 2
-
-    let y = centerY - squareSize * rowsCount / 2
-
     // In fact, we can optimize the algorithm by not generating the whole
     // Pascal triangle, but instead just check if numbers are even or odd
-    for (let row of pascalTriangle(0n, BigInt(rowsCount - 1))) {
+    for (let row of pascalTriangle(bigInt(startingRow - 1), bigInt(rowsCount - 1))) {
         // Calculate the most left square coordinate
-        let itemX = centerX - Math.floor(squareSize * row.length / 2)
+        let itemX = x - Math.floor(squareSize * row.length / 2)
 
         for (let number of row) {
             // If the number in Pascal triangle is odd,
@@ -105,7 +100,97 @@ const draw = () => {
         // Update the Y coordinate for the next row
         y += squareSize
     }
+}
 
+
+let lastUpdateTime = Date.now()
+let deltaTime = 0
+
+let topPartSize = 1
+
+const getFPS = () => 1000 / deltaTime
+
+// Function that is called on each frame
+const draw = () => {
+    deltaTime = Date.now() - lastUpdateTime
+    const { width, height } = canvas
+    
+    // Fill the background with black
+    context.fillStyle = 'black'
+    context.strokeStyle = 'transparent'
+    context.fillRect(0, 0, width, height)
+
+    // Set the fill color of our triangle
+    context.fillStyle = 'white'
+
+    // Coordinates of the center of the screen
+    const centerX = width / 2
+    const centerY = height / 2
+
+    // Top triangle
+    const topSquareSize = defaultSquareSize * topPartSize
+    const topHeight = heightOfTriangle(rowsCount, topSquareSize)
+    const topWidth = widthOfLine(rowsCount, topSquareSize)
+    const topY = 0
+    const topX = 0
+
+    // Bottom part: two triangles
+    const bottomSquareSize = defaultSquareSize * (1 - topPartSize)
+    const bottomHeight = heightOfTriangle(rowsCount, bottomSquareSize)
+    const bottomWidth = widthOfLine(rowsCount, bottomSquareSize)
+
+    // Left triangle
+    const leftX = topX - topWidth / 2
+    const leftY = topHeight
+
+    // Right triangle
+    const rightX = topX + topWidth / 2
+    const rightY = leftY
+
+    const y = centerY - (bottomHeight + topHeight) / 2
+
+    context.save()
+    context.translate(centerX, y)
+
+    drawTriangleAt({
+        x: topX, 
+        y: topY, 
+        rowsCount,
+        squareSize: topSquareSize
+    })
+
+    drawTriangleAt({
+        x: leftX,
+        y: leftY,
+        rowsCount,
+        squareSize: bottomSquareSize
+    })
+
+    drawTriangleAt({
+        x: rightX, 
+        y: rightY,
+        rowsCount,
+        squareSize: bottomSquareSize
+    })
+
+    // Finish the border around the triangle
+    drawTriangleAt({
+        x: topX,
+        y: leftY + bottomHeight,
+        startingRow: rowsCount * 2,
+        rowsCount: rowsCount * 2,
+        squareSize: defaultSquareSize / 2
+    })
+
+    context.restore()
+
+    topPartSize -= Math.min(50, Math.max(10, deltaTime)) / 50 / 10
+
+    if (topPartSize < 0.5) {
+        topPartSize = 1
+    }
+
+    lastUpdateTime = Date.now()
     requestAnimationFrame(draw)
 }
 
